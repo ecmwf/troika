b68412095fa5a9e9a8b7258f233e979cc876ee24
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2021-05-06 13:40:12 +0100

    Add Connection.sendfile method

--- src/troika/connection.py
+++ src/troika/connection.py
@@ -1,10 +1,13 @@
 """Abstraction of the way to connect to a host"""
 
 import logging
+import os
+import shutil
 import subprocess
 from subprocess import DEVNULL, STDOUT, PIPE
 
-from . import ConfigurationError
+from . import ConfigurationError, RunError
+from .utils import check_status
 
 _logger = logging.getLogger(__name__)
 
@@ -56,6 +59,21 @@ class Connection:
         """
         raise NotImplementedError
 
+    def sendfile(self, src, dst, dryrun=False):
+        """Copy the given file to the remote host
+
+        Parameters
+        ----------
+        src: path-like
+            Path to the file on the local host
+        dst: path-like
+            Path to the target directory or file on the remote host
+        dryrun: bool
+            If True, do not do anything but print the command that would be
+            executed
+        """
+        raise NotImplementedError
+
 
 class LocalConnection(Connection):
     """Connection to the local host"""
@@ -67,12 +85,12 @@ class LocalConnection(Connection):
         return f"{self.__class__.__name__}()"
 
     def is_local(self):
-        """See `troika.connection.Connection.is_local"""
+        """See `troika.connection.Connection.is_local`"""
         return True
 
     def execute(self, command, stdin=None, stdout=None, stderr=None,
             detach=False, dryrun=False):
-        """See `troika.connection.Connection.execute"""
+        """See `troika.connection.Connection.execute`"""
         if dryrun:
             _logger.info("Execute: %s", " ".join(repr(str(arg)) for arg in command))
             return
@@ -88,6 +106,13 @@ class LocalConnection(Connection):
         _logger.debug("Child PID: %d", proc.pid)
         return proc.pid
 
+    def sendfile(self, src, dst, dryrun=False):
+        """See `troika.connection.Connection.sendfile`"""
+        if not dryrun:
+            shutil.copy(src, dst)
+        else:
+            _logger.info("Copying %r to %r", src, dst)
+
 
 class SSHConnection(Connection):
     """Connection to a remote host via SSH"""
@@ -96,6 +121,7 @@ class SSHConnection(Connection):
         super().__init__(config, user)
         self.parent = LocalConnection({}, user)
         self.ssh = config.get('ssh_command', 'ssh')
+        self.scp = config.get('scp_command', 'scp')
         self.host = config['host']
 
     def __repr__(self):
@@ -103,12 +129,30 @@ class SSHConnection(Connection):
 
     def execute(self, command, stdin=None, stdout=None, stderr=None,
             detach=False, dryrun=False):
-        """See `troika.connection.Connection.execute"""
+        """See `troika.connection.Connection.execute`"""
         ssh_args = [self.ssh, '-v', '-o', 'StrictHostKeyChecking=no',
             '-l', self.user, self.host]
         args = ssh_args + command
         return self.parent.execute(args, stdin=stdin, stdout=stdout,
             stderr=stderr, detach=detach, dryrun=dryrun)
+
+    def sendfile(self, src, dst, dryrun=False):
+        """See `troika.connection.Connection.sendfile`"""
+        scp_args = [self.scp, '-v', '-o', 'StrictHostKeyChecking=no', src,
+            f"{self.user}@{self.host}:{dst}"]
+        pid = self.parent.execute(scp_args, dryrun=dryrun)
+        if dryrun:
+            return
+        _, sts = os.waitpid(pid, 0)
+        retcode = check_status(sts)
+        if retcode != 0:
+            msg = "Copy "
+            if retcode > 0:
+                msg += f"failed with exit code {retcode}"
+            else:
+                msg += f"terminated by signal {-retcode}"
+            msg += f", check {str(sub_output)!r}"
+            raise RunError(msg)
 
 
 _CONNECTIONS = {
