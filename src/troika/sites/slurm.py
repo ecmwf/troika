6b59a3ecaa64f727025a98ac4ca684f54cee10dd
Zak Kipling <zak.kipling@ecmwf.int>
2022-02-24 17:05:39 +0000

    Allow signal to be name not number

--- src/troika/sites/slurm.py
+++ src/troika/sites/slurm.py
@@ -252,7 +252,7 @@ class SlurmSite(Site):
 
             cmd = [self._scancel, str(jid)]
             if sig is not None:
-                cmd.extend(["-s", str(int(sig))])
+                cmd.extend(["-s", str(sig)])
             proc = self._connection.execute(cmd, stdout=PIPE, dryrun=dryrun)
 
             if dryrun:
