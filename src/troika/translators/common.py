d76b218db3a1114084a8455fa2533b8ce639b403
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2022-09-30 15:29:24 +0000

    Support extra directive set in the configuration file

--- src/troika/translators/common.py
+++ src/troika/translators/common.py
@@ -6,3 +6,10 @@ def join_output_error(script_data, global_config, site):
     if 'error_file' not in script_data['directives']:
         script_data['directives']['join_output_error'] = ()
     return script_data
+
+
+def extra_directives(script_data, global_config, site):
+    """Add the content of 'extra_directives' in the site configuration"""
+    for name, val in site.config.get('extra_directives', {}).items():
+        script_data['directives'].setdefault(name, val)
+    return script_data
