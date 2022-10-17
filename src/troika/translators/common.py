f39484b29827f3e3fe209a13ad733db7b3eb41f5
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2022-10-17 15:30:43 +0000

    Check type in extra_directives

--- src/troika/translators/common.py
+++ src/troika/translators/common.py
@@ -1,5 +1,7 @@
 """Common directive translators"""
 
+from .. import ConfigurationError
+
 
 def join_output_error(script_data, global_config, site):
     """Add the 'join_output_error' directive when needed"""
@@ -11,5 +13,10 @@ def join_output_error(script_data, global_config, site):
 def extra_directives(script_data, global_config, site):
     """Add the content of 'extra_directives' in the site configuration"""
     for name, val in site.config.get('extra_directives', {}).items():
+        if isinstance(val, (int, float)):
+            val = str(val)
+        if not isinstance(val, str):
+            raise ConfigurationError(
+                f"Invalid value type for directive {name!r}: {val!r}")
         script_data['directives'].setdefault(name, val.encode("utf-8"))
     return script_data
