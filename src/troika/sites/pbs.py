be106b2e4d3d69832af0847888cb56254d9d09a9
map <24995792+morianemo@users.noreply.github.com>
2025-11-25 14:54:23 +0000

    INT-1230

--- src/troika/sites/pbs.py
+++ src/troika/sites/pbs.py
@@ -115,6 +115,10 @@ class PBSSite(Site):
         "priority": b"-p %s",
         "queue": b"-q %s",
         "walltime": b"-l walltime=%s",
+        "memsz_prc": b"-l memsz_prc=%s",
+        "vmemsz_prc": b"-l vmemsz_prc=%s",
+        "memsz_job": b"-l memsz_job=%s",
+        "vmemsz_job": b"-l vmemsz_job=%s",
     }
 
     def __init__(self, config, connection, global_config):
